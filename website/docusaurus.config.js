335dc26768f19d76acd104e059416641e605ed15
canciller <lopez.ojeda.gabriel.emilio@gmail.com>
2022-05-31 09:37:27 -0500

    Antd remove global styles workaround
    - Copy antd.css from node_modules to src/css.
    - Comment or remove global styles.

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -47,7 +47,10 @@ const config = {
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: [
+            require.resolve('./src/css/antd.css'),
+            require.resolve('./src/css/custom.css'),
+          ]
         },
       }),
     ],
