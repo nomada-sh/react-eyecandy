ec8931734cfd41283e90849af3231e0ac89a13aa
canciller <lopez.ojeda.gabriel.emilio@gmail.com>
2022-06-02 16:53:09 -0500

    Added antd build script and theme switch example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,9 +1,22 @@
 import { Thing } from '@nomada-sh/react-eyecandy'
+import '@nomada-sh/react-eyecandy/dist/eyecandy.css';
+import { Button } from 'antd';
+import React from 'react';
 
 export function App() {
+  const [dark, setDark] = React.useState(false);
+
   return (
     <div>
-      <Thing />
+      <Button
+        type="primary"
+        onClick={() => {
+          document.documentElement.style.setProperty('--ant-background-color', dark ? '#fff' : '#000');
+          setDark(!dark);
+        }}
+      >
+        Toggle Theme
+      </Button>
     </div>
   );
 }
