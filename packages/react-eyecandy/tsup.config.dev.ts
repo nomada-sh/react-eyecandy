f331c7313c148b9e8c134ccd8af1eddacbe0de16
canciller <lopez.ojeda.gabriel.emilio@gmail.com>
2022-06-10 10:24:56 -0500

    Created start:react-eyecandy script and fixed linter errors

--- packages/react-eyecandy/tsup.config.dev.ts
+++ packages/react-eyecandy/tsup.config.dev.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'tsup'
+import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['src'],
@@ -6,4 +6,4 @@ export default defineConfig({
   sourcemap: 'inline',
   clean: true,
   dts: true,
-})
+});
