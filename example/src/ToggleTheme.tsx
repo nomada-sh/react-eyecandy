9bb173986e0e624901b07d1f73d1752eebc871e2
canciller <lopez.ojeda.gabriel.emilio@gmail.com>
2022-06-13 08:41:12 -0500

    Added text examples

--- example/src/ToggleTheme.tsx
+++ example/src/ToggleTheme.tsx
@@ -1,6 +1,8 @@
 import { Button, Input, Alert, Typography } from 'antd';
 import { useTheme } from '@nomada-sh/react-eyecandy';
 
+const { Paragraph, Title } = Typography;
+
 export function ToggleTheme() {
   const { dark, toggleDark, setThemeChanges } = useTheme();
 
@@ -28,10 +30,30 @@ export function ToggleTheme() {
       />
       <Alert
         message="Error"
-        description="This is an error message about copywriting."
+        description="This is an error message."
         type="error"
         showIcon
       />
+      <Alert
+        message="Success"
+        description="This is a success message."
+        type="success"
+        showIcon
+      />
+      <Title level={1}>Title 1</Title>
+      <Paragraph>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed hic, quis
+        maiores impedit quas a ullam tempora, quisquam provident soluta saepe!
+        Assumenda deleniti reiciendis tenetur eaque, vero tempora libero
+        blanditiis? Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+        Dolore id quis, blanditiis dolores officia vitae rem qui eos, officiis
+        quia voluptate cupiditate voluptatibus iure magnam assumenda illo ex
+        corrupti voluptatem!
+      </Paragraph>
+      <Title level={2}>Title 2</Title>
+      <Title level={3}>Title 3</Title>
+      <Title level={4}>Title 4</Title>
+      <Title level={5}>Title 5</Title>
     </div>
   );
 }
